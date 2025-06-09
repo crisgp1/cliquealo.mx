ecf756ef552f437433d51929ad3eb7d86a7e30a9
Cristian G <cristiangp2001@gmail.com>
2025-06-09 16:16:45 -0600

    Remove @vercel/remix dependency and delete vercel.json configuration file; update remix.config.js for automatic output handling.

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  serverBuildTarget: "vercel",
+  serverBuildTarget: "vercel", // 💥 Remix usará .vercel/output automáticamente
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
