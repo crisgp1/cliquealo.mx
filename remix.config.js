66a5cebcc49707df838059556616f109651f687f
Cristian G <cristiangp2001@gmail.com>
2025-06-12 14:58:38 -0600

    Refactor code structure for improved readability and maintainability

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   serverBuildTarget: "vercel",
-  server: "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  appDirectory: "app",
+  appDirectory: "app", 
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
   serverBuildPath: "build/index.js",
